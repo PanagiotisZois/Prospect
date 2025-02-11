e3b4bd4b74f8ffd9733e5a4775400377ab8bb904
PanagiotisZois <zoespanagiotes@gmail.com>
2025-02-11 16:23:11 +0200

    fix for services

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
 btn.addEventListener('click', navToggle)
-linkForm.addEventListener('submit', formSubmit)
 
 // Toggle Mobile Menu
 function navToggle() {
@@ -29,20 +28,4 @@ function validURL(str) {
   return !!pattern.test(str)
 }
 
-function formSubmit(e) {
-  e.preventDefault()
 
-  console.log(input.value);
-
-  if (input.value === '') {
-    errMsg.innerHTML = 'Please enter something'
-    input.classList.add('border-red')
-  } else if (!validURL(input.value)) {
-    errMsg.innerHTML = 'Please enter a valid URL'
-    input.classList.add('border-red')
-  } else {
-    errMsg.innerHTML = ''
-    input.classList.remove('border-red')
-    alert('Success')
-  }
-}
